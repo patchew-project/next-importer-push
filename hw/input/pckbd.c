7a3db632bcd78e642221865f41783c84139beedb
Thomas Huth <thuth@redhat.com>
2019-04-04 09:14:21 +0200

    hw/input/pckbd: The i8042 device should not be user_creatable
    The i8042 PS/2 controller is part of the chipset on the motherboard.
    It is instantiated by the machine init code, and it does not make sense
    to allow the user to plug an additional i8042 in any of the free ISA slots.
    Thus let's mark the device with user_creatable = false.
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Message-Id: <20190404071421.4891-1-thuth@redhat.com>

--- hw/input/pckbd.c
+++ hw/input/pckbd.c
@@ -597,6 +597,8 @@ static void i8042_class_initfn(ObjectClass *klass, void *data)
     dc->vmsd = &vmstate_kbd_isa;
     isa->build_aml = i8042_build_aml;
     set_bit(DEVICE_CATEGORY_INPUT, dc->categories);
+    /* i8042 is a device on the motherboard, and not pluggable by the user */
+    dc->user_creatable = false;
 }
 
 static const TypeInfo i8042_info = {
