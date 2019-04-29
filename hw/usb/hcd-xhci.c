bb802d74274cd42950b60f307006be766a19ccdf
Longpeng <longpeng2@huawei.com>
2019-04-29 15:39:29 +0800

    usb/xchi: avoid trigger assertion if guest write wrong epid
    we found the following core in our environment:
    0  0x00007fc6b06c2237 in raise ()
    1  0x00007fc6b06c3928 in abort ()
    2  0x00007fc6b06bb056 in __assert_fail_base ()
    3  0x00007fc6b06bb102 in __assert_fail ()
    4  0x0000000000702e36 in xhci_kick_ep (...)
    6  0x000000000047767f in access_with_adjusted_size (...)
    7  0x000000000047944d in memory_region_dispatch_write (...)
    8  0x000000000042df17 in address_space_write_continue (...)
    10 0x000000000043084d in address_space_rw (...)
    11 0x000000000047451b in kvm_cpu_exec (cpu=cpu@entry=0x1ab11b0)
    12 0x000000000045dcf5 in qemu_kvm_cpu_thread_fn (arg=0x1ab11b0)
    13 0x0000000000870631 in qemu_thread_start (args=args@entry=0x1acfb50)
    14 0x00000000008959a7 in thread_entry_for_hotfix (pthread_cb=<optimized out>)
    15 0x00007fc6b0a60dd5 in start_thread ()
    16 0x00007fc6b078a59d in clone ()
    (gdb) bt
    (gdb) f 5
    (gdb) p /x tmp
    $9 = 0x62481a00 <-- last byte 0x00 is @epid
    
    xhci_doorbell_write() already check the upper bound of @slotid an @epid,
    it also need to check the lower bound.
    
    Cc: Gonglei <arei.gonglei@huawei.com>
    Signed-off-by: Longpeng <longpeng2@huawei.com>
    Message-Id: <1556523569-44480-1-git-send-email-longpeng2@huawei.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@redhat.com>

--- hw/usb/hcd-xhci.c
+++ hw/usb/hcd-xhci.c
@@ -3102,7 +3102,7 @@ static void xhci_doorbell_write(void *ptr, hwaddr reg,
     } else {
         epid = val & 0xff;
         streamid = (val >> 16) & 0xffff;
-        if (reg > xhci->numslots) {
+        if (reg == 0 || reg > xhci->numslots) {
             DPRINTF("xhci: bad doorbell %d\n", (int)reg);
         } else if (epid == 0 || epid > 31) {
             DPRINTF("xhci: bad doorbell %d write: 0x%x\n",
