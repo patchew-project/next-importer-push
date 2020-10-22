b18d7f0909490c42a2b5f3775ae81c14569d81e1
Tobin Feldman-Fitzthum <tobin@linux.ibm.com>
2020-10-22 00:16:30 -0400

    sev: add sev-inject-launch-secret
    AMD SEV allows a guest owner to inject a secret blob
    into the memory of a virtual machine. The secret is
    encrypted with the SEV Transport Encryption Key and
    integrity is guaranteed with the Transport Integrity
    Key. Although QEMU facilitates the injection of the
    launch secret, it cannot access the secret.
    
    Signed-off-by: Tobin Feldman-Fitzthum <tobin@linux.ibm.com>
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>
    Reviewed-by: Brijesh Singh <brijesh.singh@amd.com>
    Message-Id: <20201022041630.33690-1-tobin@linux.ibm.com>

--- monitor/misc.c
+++ monitor/misc.c
@@ -667,10 +667,10 @@ static void hmp_physical_memory_dump(Monitor *mon, const QDict *qdict)
     memory_dump(mon, count, format, size, addr, 1);
 }
 
-static void *gpa2hva(MemoryRegion **p_mr, hwaddr addr, Error **errp)
+void *gpa2hva(MemoryRegion **p_mr, hwaddr addr, uint64_t size, Error **errp)
 {
     MemoryRegionSection mrs = memory_region_find(get_system_memory(),
-                                                 addr, 1);
+                                                 addr, size);
 
     if (!mrs.mr) {
         error_setg(errp, "No memory is mapped at address 0x%" HWADDR_PRIx, addr);
@@ -681,6 +681,11 @@ static void *gpa2hva(MemoryRegion **p_mr, hwaddr addr, Error **errp)
         error_setg(errp, "Memory at address 0x%" HWADDR_PRIx "is not RAM", addr);
         memory_region_unref(mrs.mr);
         return NULL;
+    }
+
+    if (mrs.size < size) {
+        error_setg(errp, "Size of memory region at 0x%" HWADDR_PRIx
+                   " exceeded.", addr);
     }
 
     *p_mr = mrs.mr;
@@ -694,7 +699,7 @@ static void hmp_gpa2hva(Monitor *mon, const QDict *qdict)
     MemoryRegion *mr = NULL;
     void *ptr;
 
-    ptr = gpa2hva(&mr, addr, &local_err);
+    ptr = gpa2hva(&mr, addr, 1, &local_err);
     if (local_err) {
         error_report_err(local_err);
         return;
@@ -770,7 +775,7 @@ static void hmp_gpa2hpa(Monitor *mon, const QDict *qdict)
     void *ptr;
     uint64_t physaddr;
 
-    ptr = gpa2hva(&mr, addr, &local_err);
+    ptr = gpa2hva(&mr, addr, 1, &local_err);
     if (local_err) {
         error_report_err(local_err);
         return;
